410a06fad49b95f457ffcdb26c48fa3cb2bac9db
Samuel Bergman <samuel-bergman@hotmail.com>
2024-05-07 21:07:54 -0300

    projetos classes e tipos variaveis

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,17 @@
 public class MinhaClasse {
     
     public static void main(String[] args) {
-       System.out.print("Olá, Mundo!");
+       String primeiroNome = "Samuel";
+       String segundoNome = "Souza";
+
+       String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+       System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome)
+    {
+        return "Resultado do método: " +  primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
