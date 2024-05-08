4dc8c1ed53f771327a53821e7a6e5bfdb645a602
Samuel Bergman <samuel-bergman@hotmail.com>
2024-05-08 02:07:09 -0300

    aula tipos variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      int a, b;
+
+      a = 6;
+      b = 6;
+
+      String resultado = a == b ? "verdadeiro" : "falso";
+
+      /* if (a==b) resultado = "verdadeiro";
+      else resultado = "falso";*/
+
+      System.out.println(resultado);
     }
+    
 }
